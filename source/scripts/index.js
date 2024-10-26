c32f8c5ffa34ac0d8019bd30fa1648f69eba9254
webdevandreev <webdevandreev@gmail.com>
2024-10-26 23:22:51 +0300

    "Refactor header script and styles: toggle nav class instead of display property, update button and nav display rules, and add media queries for different screen sizes."

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,4 +1,3 @@
-/* в этот файл добавляет скрипты*/
 // Получаем элементы из DOM
 const button = document.querySelector('.header__button');
 const nav = document.querySelector('.header__nav');
@@ -6,9 +5,5 @@ const nav = document.querySelector('.header__nav');
 // Добавляем обработчик события на кнопку
 button.addEventListener('click', () => {
   // Проверяем текущее состояние навигации
-  if (nav.style.display === 'block') {
-    nav.style.display = 'none'; // Скрываем навигацию
-  } else {
-    nav.style.display = 'block'; // Показываем навигацию
-  }
+  nav.classList.toggle('header__nav--active'); // Переключаем класс активности
 });
