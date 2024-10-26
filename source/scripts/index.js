ec9c897ad3bc208f16e7928481c94137e345e77f
webdevandreev <webdevandreev@gmail.com>
2024-10-27 00:21:14 +0300

    "Updated index.html, index.js, header.scss, and page.scss files: modified header layout, removed JavaScript code, and changed CSS styles and variables."

--- source/scripts/index.js
+++
@@ -1,8 +0,0 @@
-// Получаем элементы из DOM
-const button = document.querySelector('.header__button');
-const nav = document.querySelector('.header__nav');
-
-// Добавляем обработчик события на кнопку
-button.addEventListener('click', () => {
-  nav.classList.toggle('header__nav--active'); // Переключаем класс активности
-});
