358a89856a34c25eae4a576a5980476e90994118
webdevandreev <webdevandreev@gmail.com>
2024-10-26 23:17:26 +0300

    "Removed Inter font files and CSS, updated header and navigation styles, and modified global styles and variables."

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,14 @@
 /* в этот файл добавляет скрипты*/
+// Получаем элементы из DOM
+const button = document.querySelector('.header__button');
+const nav = document.querySelector('.header__nav');
+
+// Добавляем обработчик события на кнопку
+button.addEventListener('click', () => {
+  // Проверяем текущее состояние навигации
+  if (nav.style.display === 'block') {
+    nav.style.display = 'none'; // Скрываем навигацию
+  } else {
+    nav.style.display = 'block'; // Показываем навигацию
+  }
+});
