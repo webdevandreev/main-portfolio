e567502ebbf7402284f2a62b1fb110fdc95632f7
Aleskey Andreev <alexmilaylia@yandex.ru>
2024-10-31 18:19:46 +0300

    "Updated .editorconfig, index.html, index.js, header.scss, main.scss, and page.scss files with various changes to HTML structure, CSS styles, and JavaScript functionality."

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,8 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
   const button = document.querySelector('.header__button');
   const nav = document.querySelector('.header__nav');
+  const contentWrapper = document.querySelector('.content-wrapper');
 
   button.addEventListener('click', () => {
-    nav.classList.toggle('visible'); // Переключаем класс 'visible'
+    nav.classList.toggle('visible');
+    contentWrapper.classList.toggle('nav-open');
   });
 });
