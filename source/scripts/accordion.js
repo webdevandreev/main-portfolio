0b52a479fe2761b4dabb41cee515c9da54b81e57
webdevandreev <webdevandreev@gmail.com>
2024-11-11 19:53:40 +0300

    "Updated index.html, accordion.js, and skills.scss with various changes to HTML structure, script functionality, and CSS styles."

--- source/scripts/accordion.js
+++ source/scripts/accordion.js
@@ -4,18 +4,14 @@ skillsListItems.forEach((item) => {
   const info = item.querySelector('.skills__list');
 
   item.addEventListener('mouseenter', () => {
-    info.classList.add('show'); // Показать дополнительную информацию
+    if (info) {
+      info.classList.add('show'); // Показать дополнительную информацию
+    }
   });
 
   item.addEventListener('mouseleave', () => {
-    info.classList.remove('show'); // Скрыть дополнительную информацию
-  });
-
-  item.addEventListener('focusin', () => {
-    info.classList.add('show'); // Показать дополнительную информацию при фокусе
-  });
-
-  item.addEventListener('focusout', () => {
-    info.classList.remove('show'); // Скрыть дополнительную информацию при потере фокуса
+    if (info) {
+      info.classList.remove('show'); // Скрыть дополнительную информацию
+    }
   });
 });
