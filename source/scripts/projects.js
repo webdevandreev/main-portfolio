d68ef72192c073d2eb1353fbadda10db950aadd9
Aleskey Andreev <alexmilaylia@yandex.ru>
2025-01-11 19:59:16 +0300

    `Added delay to popup activation and added CSS rule for img[alt]`

--- source/scripts/projects.js
+++ source/scripts/projects.js
@@ -1,13 +1,35 @@
 const block = document.querySelector('.projects__block');
 const popup = document.querySelector('.projects__popup');
+const ctaButton = document.querySelector('.projects__cta');
+const closeBtn = document.createElement('button'); // Создаем кнопку закрытия
+
+// Добавляем класс и текст для кнопки закрытия
+closeBtn.classList.add('close-btn');
+popup.appendChild(closeBtn); // Вставляем кнопку в попап
 
 block.addEventListener('touchstart', () => {
   popup.classList.add('active');
+  block.style.pointerEvents = 'none'; // добавляем это, чтобы избежать immediate клика на кнопке
+  setTimeout(() => {
+    block.style.pointerEvents = 'auto'; // восстанавливаем pointerEvents через 200 миллисекунд
+  }, 200);
+  document.body.style.overflowY = 'hidden'; // Остановка прокрутки страницы по вертикали
 });
 
-// Закрытие попапа при клике вне области
-document.addEventListener('touchstart', (event) => {
-  if (!popup.contains(event.target) && !block.contains(event.target)) {
+popup.addEventListener('touchend', (event) => {
+  if (event.target.closest('.projects__popup') === popup) {
     popup.classList.remove('active');
+    document.body.style.overflowY = 'auto'; // Восстановление прокрутки страницы по вертикали
   }
+});
+
+ctaButton.addEventListener('touchstart', (event) => {
+  event.preventDefault(); // предотвращаем стандартное поведение кнопки
+  window.open(ctaButton.href, '_blank'); // открываем ссылку в новом окне
+});
+
+// Обработчик для закрытия попапа по клику на крестик
+closeBtn.addEventListener('click', () => {
+  popup.classList.remove('active');
+  document.body.style.overflowY = 'auto'; // Восстановление прокрутки страницы по вертикали
 });
